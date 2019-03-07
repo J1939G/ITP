9476a1adac07c4aa0bbdbf516086b20e41226067
Jose <jgredonsky@gmail.com>
2019-03-07 13:55:56 +0100

    Added historie.header

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <?php if( $pagename == 'priroda'): echo '<link rel="stylesheet" href="' . get_stylesheet_directory_uri() . '/styles/nature.css" />'; endif;?>
     <noscript><link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() . '/styles/noscript.css' ?>" ></noscript>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php echo '<style rel="stylesheet" type="text/css">@media only screen and ( min-aspect-ratio: 3/4) {#navigation .menu-page-container {background-image: url(' . get_stylesheet_directory_uri() . '/images/' . $pagename . '/' . $pagename . '_header.jpg);}}</style>';
+    <?php echo '<style rel="stylesheet" type="text/css">@media only screen and ( min-aspect-ratio: 3/4) {#navigation .menu-page-container {background-image: url(' . get_stylesheet_directory_uri() . '/images/headers/' . $pagename . '_header.jpg);}}</style>';
     wp_head(); 
     ?>
     
--- page-archive.php
+++ page-archive.php
@@ -11,14 +11,14 @@
     <?php echo '<link rel="stylesheet" href="' . get_stylesheet_directory_uri() . '/styles/posts.css" />';?>
     <?php echo '<script src="' . get_stylesheet_directory_uri() . '/scripts/jose.js" ></script>';?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php echo '<style rel="stylesheet" type="text/css">@media only screen and ( min-aspect-ratio: 3/4) {#navigation .menu-page-container {background-image: url(' . get_stylesheet_directory_uri() . '/images/' . $pagename . '/' . $pagename . '_header.jpg);}}</style>';
+    <?php echo '<style rel="stylesheet" type="text/css">@media only screen and ( min-aspect-ratio: 3/4) {#navigation .menu-page-container {background-image: url(' . get_stylesheet_directory_uri() . '/images/headers/' . $pagename . '_header.jpg);}}</style>';
     wp_head(); 
     ?>
 </head>
 <body>
     <div id="content">
         <nav id="navigation">
-            <h1 id="title" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/<?php echo $pagename ?>/<?php echo $pagename ?>_header.jpg);"><?php echo wp_title(""); ?></h1>
+            <h1 id="title" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/headers/<?php echo $pagename ?>_header.jpg);"><?php echo wp_title(""); ?></h1>
             <div onclick="menu('menu-page-container')" id="arrow"></div>
             <div class="menu-page-container">
                 <ul id="menu-page" class="menu">
